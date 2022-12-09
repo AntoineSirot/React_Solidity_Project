2b70694cb152640feff7131e36bd66eea6a188a2
AntoineSirot <sirot_antoine@bbox.fr>
2022-12-09 01:25:02 +0100

    update app

--- ux_tderc721_app/src/App.js
+++ ux_tderc721_app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 import AppRoutes from "./components/Routes";
+//import { Connexion } from  "./components/pages/Connexion"
 
 function App() {
 
 
     return (
+
       <div className="App">
           <Router >
             <AppRoutes />
