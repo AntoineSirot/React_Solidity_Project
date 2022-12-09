4e5b1ca03d491ddffb9564d3f5d46906d72323eb
AntoineSirot <sirot_antoine@bbox.fr>
2022-12-09 01:25:53 +0100

    update part2 (Bayc)

--- ux_tderc721_app/src/components/pages/FakeBayc.js
+++ ux_tderc721_app/src/components/pages/FakeBayc.js
@@ -1,16 +1,83 @@
 import './../../App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-import AppRoutes from "./../Routes";
+import { useEffect, useState } from 'react';
+
+
+var Web3 = require('web3');
+const web3 = new Web3(window.ethereum)
+
 
 function FakeBayc() {
+
+    const [intialized, setInitiliaze] = useState(false);
+    const [tokenName, setTokenName] = useState("");
+    const [tokenNumber, setTokenNumber] = useState("");
+    const [currentBalance, setBalance] = useState("");
+    const [tokenId, setToken] = useState(0);
+
+
+    useEffect(() => {
+      getContractInfos()
+    }, [currentBalance]);
+
+
+    const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimAToken","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokenCounter","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+    const contract_address = "0x1dA89342716B14602664626CD3482b47D5C2005E"
+
+    const contract = new web3.eth.Contract(ABI, contract_address)
+
+
+    async function claimToken() {
+      const accounts = await window.ethereum.request
+      ({
+        method: "eth_requestAccounts",
+      });
+      await contract.methods.claimAToken().send({from: accounts[0]})
+    }
+
+
+    async function getContractInfos() {
+      const accounts = await window.ethereum.request
+      ({
+        method: "eth_requestAccounts",
+      });
+      const name = await contract.methods.name().call()
+      setTokenName(name)
+      const number = await contract.methods.totalSupply().call()
+      setTokenNumber(number)
+      const balance = await contract.methods.balanceOf(accounts[0]).call()
+      setBalance(balance)
+    }
+
+    const handleInput = event => {
+      setToken(event.target.value);
+      setInitiliaze(true)
+    };
+
+    function getTokenDetail() {
+      if (intialized == true) {
+        window.open('/fakeBayc/' + tokenId, "_self")
+      }
+      else {
+        console.log("Please enter a value before submition")
+      }
+    }
 
 
     return (
       <div className="App">
           <h2>Fake Bayc</h2>
-          <a href="/">
+          <p><a href="/">
               <button>Go back to the main page here</button> 
-          </a>
+          </a></p>
+          <h2> Name : {tokenName}</h2>
+          <h2> Total Number of Token : {tokenNumber}</h2>
+          <button onClick={claimToken}> Claim a Token </button>
+          <h2> Balance of Token : {currentBalance}</h2>
+          <div>
+            <input onChange={handleInput} placeholder="Enter tokenId.."/>
+            <button onClick={getTokenDetail}>See information about the Bored Ape</button>
+          </div>
+
       </div>
     );
   }
--- ux_tderc721_app/src/components/pages/FakeBaycTokenInfo.js
+++ ux_tderc721_app/src/components/pages/FakeBaycTokenInfo.js
@@ -1,17 +1,97 @@
 import './../../App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-import AppRoutes from "./../Routes";
+import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import {IpfsImage} from "react-ipfs-image";
+
+var Web3 = require('web3');
+const web3 = new Web3(window.ethereum)
 
 function FakeBaycTokenInfo() {
+
+
+    const [error, setError] = useState("");
+    const [infos, setInfos] = useState("");
+    const [image, setImage] = useState("");
+    const [token, setToken] = useState(0);
+    const [intialized, setInitiliaze] = useState(false);
+
+
+
+    const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimAToken","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokenCounter","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+    const contract_address = "0x1dA89342716B14602664626CD3482b47D5C2005E"
+
+    const contract = new web3.eth.Contract(ABI, contract_address)
+
+    let {tokenId} = useParams();
+
+
+
+    useEffect(() => {
+      getTokenInfo()
+    }, []);
+    
+
+    
+
+    async function getTokenInfo() {
+      
+      const number = await contract.methods.totalSupply().call()
+      if (parseInt(tokenId) >= number) {
+        setError(<h1>! This token does not exists !</h1>)
+      }
+      else {
+        setError("")
+        const uri = await contract.methods.tokenURI(tokenId).call();
+        const urijson = await fetch(uri).then(x => x.json());
+        let image = await urijson.image
+        image = (<><h3>Bored Ape #{ tokenId }</h3>
+        <IpfsImage hash={image}/>
+        </>);
+        let s = ""
+        for (let i = 0; i < urijson.attributes.length; i++) {
+          s += urijson.attributes[i].trait_type + " : " + urijson.attributes[i].value + "; "
+        } 
+        setInfos(s)
+        setImage(image);
+      }
+    }
+
+    const handleInput = event => {
+      setToken(event.target.value);
+      setInitiliaze(true)
+    };
+
+    function getTokenDetail() {
+      if (intialized == true) {
+        window.open('/fakeBayc/' + token, "_self")
+      }
+      else {
+        console.log("Please enter a value before submition")
+      }
+    }
+
+    function Main_Page() {
+      window.open("/", "_self");
+    }
+
 
     return (
       <div className="App">
           <h2>Fake Bayc Token Info</h2>
-          <a href="/">
-              <button>Go back to the main page here</button> 
-          </a>
+          <p>
+              <button  onClick={Main_Page}>Go back to the main page here</button> 
+          </p>
+          {error}
+          {infos}
+          {image}
+          <h3> Other Bored Ape : </h3>
+          <input onChange={handleInput} placeholder="Enter tokenId.."/>
+            <button onClick={getTokenDetail}>See information about the Bored Ape</button>
+
+
       </div>
     );
+
   }
 
 export default FakeBaycTokenInfo;
