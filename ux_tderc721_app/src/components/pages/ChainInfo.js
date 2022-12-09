01e5a211b7e11543d3a6370a5c4c020960caf9c2
AntoineSirot <sirot_antoine@bbox.fr>
2022-12-09 01:25:32 +0100

    udpate Part1 (ChainInfo)

--- ux_tderc721_app/src/components/pages/ChainInfo.js
+++ ux_tderc721_app/src/components/pages/ChainInfo.js
@@ -1,18 +1,78 @@
 import './../../App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-import AppRoutes from "./../Routes";
+import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+var Web3 = require('web3');
+const web3 = new Web3(window.ethereum)
+
+
+
 
 function ChainInfo() {
 
 
+
+  const [walletAddress, setWalletAddress] = useState("");
+  const [chainID, setChainID] = useState("");
+  const [lastBlock, setLastBlock] = useState("");
+  const [root, setRoot] = useState("");
+
+  
+
+    //string s
+    //s.match("yes : \w+")
+
+
+    
+    useEffect(() => {
+      getInfos()
+    }, [chainID]);
+
+    useEffect(() => {
+      if (window.ethereum) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+      }
+    });
+
+
+    
+
+    async function getInfos() {
+      const accounts = await window.ethereum.request
+      ({
+        method: "eth_requestAccounts",
+      });
+     const ID = await web3.eth.getChainId();
+     setChainID(ID);
+     const BlockNumber = await web3.eth.getBlockNumber();
+     setLastBlock(BlockNumber);
+     setWalletAddress(accounts[0]);
+     console.log({chainID})
+     if (chainID != '' & chainID != '11155111') // Looking at chainID != '' because ID = await web3.eth.getChainId(); so we need to know that it's done before veryfing it
+      {
+      console.log('Wrong Network !')
+      setRoot(<Navigate replace to="./../WrongNetwork" />)
+      
+     }
+    }
+    
     return (
-      <div className="App">
-        <h2>Chain Info</h2>
+      <div className="ChainInfo">
+          <h2>Chain Info</h2>
           <a href="/">
               <button>Go back to the main page here</button> 
           </a>
+          <p>Chain ID : {chainID}</p>
+          <p>Address : {walletAddress}</p>
+          <p>Last Block : {lastBlock}</p>
+          {root}
       </div>
     );
-  }
+
+    
+    }
+
 
 export default ChainInfo;
--- ux_tderc721_app/src/components/pages/WrongNetwork.js
+++ ux_tderc721_app/src/components/pages/WrongNetwork.js
@@ -1,6 +1,5 @@
 import './../../App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-import AppRoutes from "./../Routes";
+
 
 function WrongNetwork () {
 
@@ -8,6 +7,7 @@ function WrongNetwork () {
     return (
       <div className="App">
           <h2>Wrong Network</h2>
+          <h3>It seems that you're on the wrong network, please switch to Sepolia</h3>
           <a href="/">
               <button>Go back to the main page here</button> 
           </a>
